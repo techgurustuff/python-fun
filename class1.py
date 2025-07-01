13ebf74ce37263b54bd4c23c4ee6fd042fab0579
techgurustuff <79404603+techgurustuff@users.noreply.github.com>
2025-07-01 07:44:15 +0530

    Create class1.py
    variables and datatype

---
+++ class1.py
@@ -0,0 +1,35 @@
+x = 42
+print('The value of x is', x)
+print('The type of x is', type(x))
+print(id(x))  # id() returns the memory address of the object
+y= 3.14
+print('The value of y is', y)   
+print('The type of y is', type(y))
+print(id(y))  # id() returns the memory address of the object   exit
+flag = True
+print('The value of flag is', flag)
+print('The type of flag is', type(flag))
+print(id(flag))  # id() returns the memory address of the object
+start = None
+print('The value of start is', start)   
+print('The type of start is', type(start))
+print(id(start))  # id() returns the memory address of the object
+str = 'Hello, World!'
+print('The value of str is', str)
+print('The value of str is', str[0])
+print('The type of str is', type(str))
+print(id(str))  # id() returns the memory address of the object
+lst = [10, 20, 30, 45, 55]
+print('The value of lst is', lst[4])   
+print('The type of lst is', type(lst))
+print(id(lst))  # id() returns the memory address of the object
+z =2 + 3j
+print('The value of z is', z.real, ' + ', z.imag, 'j')       
+print('The type of z is', type(z))
+print(id(z))  # id() returns the memory address of the object   
+
+# Tuple
+tup = (1, 2, 3, 4, 5)       
+print('The value of tup is', tup[2])
+print('The type of tup is', type(tup))          
+print(id(tup))  # id() returns the memory address of the object
