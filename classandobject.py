6207665a866c2904921c8550fd0dac6c5614bb5a
techgurustuff <79404603+techgurustuff@users.noreply.github.com>
2025-07-02 08:31:26 +0530

    Create classandobject.py
    class and object

---
+++ classandobject.py
@@ -0,0 +1,19 @@
+# define a class
+class Bike:
+    name = "Road Bike"  # class attribute
+    gear = 0
+
+# create object of class
+bike1 = Bike()
+
+# access attributes and assign new values
+bike1.gear = 5
+bike1.name = "Mountain Bike"
+
+print(f"Name: {bike1.name}, Gears: {bike1.gear} ")
+
+bike2 = Bike()
+# access attributes and assign new values
+
+
+print(f"Name: {bike2.name}, Gears: {bike2.gear} ")
