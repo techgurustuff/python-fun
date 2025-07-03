2b3e365884f9b412d1e0477922a5b18f48f70b72
techgurustuff <79404603+techgurustuff@users.noreply.github.com>
2025-07-03 08:11:17 +0530

    Update classandobject.py
    add methods for class

--- classandobject.py
+++ classandobject.py
@@ -17,3 +17,26 @@ bike2 = Bike()
 
 
 print(f"Name: {bike2.name}, Gears: {bike2.gear} ")
+
+class Employee:
+    id = 0  # class attribute
+    name = ""  # class attribute
+    
+    #constructor method
+    def __init__(self, name, id):
+        self.name = name  # instance attribute
+        self.id = id    # instance attribute
+
+    #class Method
+    def display_info(self):
+        print(f"Inside Class Employee ID: {self.id}")  # instance method to display employee ID  
+
+
+
+emp1 = Employee("Ram",  101)  # creating an instance of Employee
+#emp1.id = 101  # instance attribute
+emp1.display_info()  # calling the instance method
+print(f"Employee ID: {emp1.id}")
+emp2 = Employee("Sarath",  102)  # creating another instance of Employee#emp2.id = 102  # instance attribute
+emp2.display_info()  # calling the instance method
+print(f"Employee ID: {emp2.id}")
